f9641728cc85daf1440aacc0c5cf0a869a4126f1
Angel Sullon <asullom@gmail.com>
2014-04-07 11:18:01 -0500

    initial test

---
+++ src/as/HolaGit.java
@@ -0,0 +1,12 @@
+package as;
+
+public class HolaGit {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hola");
+		// Linea 1
+		// Linea 2
+	}
+
+}
