b9532e8330377cc422b1b026a9fbe83d5b6c11b5
Angel Sullon <asullom@gmail.com>
2014-04-07 16:17:11 -0500

    E

--- src/as/HolaGit.java
+++ src/as/HolaGit.java
@@ -5,12 +5,9 @@ public class HolaGit {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hola");
-		// Linea 1
-
-		// Linea 2 Local
-		// Linea 2
-		// Linea 2 LOCAL
-		// Linea 3 
+		// Linea angel.1
+		// Linea angel.2
+		// Linea angel.3
 	}
 
 }
